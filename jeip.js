115ebad4dc31b631f958f69f8edb4f0cf08fd090
joseph@josephscott.org <joseph@josephscott.org@087c43e4-aae7-11dd-b655-b92e15b8dc27>
2008-11-16 04:15:02 +0000

    Note the version change in the jeip.js file
    git-svn-id: http://jeip.googlecode.com/svn/trunk@14 087c43e4-aae7-11dd-b655-b92e15b8dc27

--- jeip.js
+++ jeip.js
@@ -21,7 +21,7 @@ OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-// version: 0.1
+// version: 0.1.2
 
 (function( $ ) {
 	$.fn.eip = function( save_url, options ) {
